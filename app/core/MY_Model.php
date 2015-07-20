a5c6aa994f3fe5e78a6cd9d771d16f3239b64ba6
Kevin Gerz <Kevin.Gerz@arcor.de>
2015-07-20 02:37:26 +0200

    Initial commit

---
+++ app/core/MY_Model.php
@@ -0,0 +1,15 @@
+<?php
+if (! defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/**
+ * Base-Model for CodeIgniter
+ *
+ * @author Kevin Gerz
+ * @copyright 2015 Kevin Gerz
+ * @license MIT License (MIT)
+ *         
+ */
+class MY_Model extends CI_Model
+{
+}
