1b8e96156f9d305436f0ce6a9c08890fc89a464b
Kevin Gerz <Kevin.Gerz@arcor.de>
2015-07-23 18:04:47 +0200

    added model description

--- app/core/MY_Model.php
+++ app/core/MY_Model.php
@@ -5,8 +5,16 @@ if (! defined('BASEPATH'))
 /**
  * Base-Model for CodeIgniter
  *
+ * This model extends CodeIgniters Model and makes it easier to
+ * interact with CodeIgniters DB-Driver.
+ * It has basic methods to easy interact with your database, such as
+ * get, insert, update or delete.
+ * Also it has build in validation, supports database relation and other funky
+ * things. (not ready yet)
+ *
  * @author Kevin Gerz
  * @copyright 2015 Kevin Gerz
+ * @see http://github.com/Urastor/CI-BaseModel Github-Project Site
  * @license MIT License (MIT)
  *         
  */
